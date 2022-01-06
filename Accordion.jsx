c397bfb5d19898f71f825118545039eabec8f14e
vipinkrishna <vipinkrishna.in@gmail.com>
2022-01-06 22:44:16 +0530

    Add: Tailwind CSS Classes

--- Accordion.jsx
+++ Accordion.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const Accordion = ({ data }) => {
     return (
-        <div>
+        <div className="accordion p-5">
             {data.map(({ header, body }) => (
                 <AccordionItem header={header} body={body} />
             ))}
@@ -13,15 +13,16 @@ const Accordion = ({ data }) => {
 
 const AccordionItem = ({ header, body }) => {
     const [show, setShow] = useState(false);
-    return (<div>
-        <div onClick={() => setShow(!show)}>
+    return (<div className="accordion-item">
+        <div onClick={() => setShow(!show)} className="accordion-header flex justify-between p-5 border text-3xl text-gray-500 select-none rounded-lg cursor-pointer hover:bg-gray-100">
             <span>{header}</span>
+            {/* <span className=""> {show ? "˅" : "˄"} </span> */}
             <span> {show ? "▽" : "△"} </span>
         </div>
 
         {
             show ? (
-                <p>
+                <p className="accordion-body p-5 border text-2xl text-gray-400 rounded-lg leading-relaxed">
                     {body}
                 </p>
             ) : ""
